879df5eab69c0f67fc1644bb1da5c3f7c8cce4cf
Oliver Steptoe <oliver.steppers@gmail.com>
2021-05-12 20:44:49 +0100

    Add West's Star Wars Matching game with spritemap fixes.

--- Star Wars Matching - West.codea/Main.lua
+++ Star Wars Matching - West.codea/Main.lua
@@ -2,16 +2,16 @@
 displayMode(FULLSCREEN_NO_BUTTONS)
 -- Use this function to perform your initial setup
 function setup()
-    
-    p1={name="Luke",score=0}
-    p2={name="Vader",score=0}    
+
+p1={name="Luke",score=0}
+p2={name="Vader",score=0}    
     activeplayer=1
     row=9
     col=4
     total=row*col
     ss=image(384,384)
     setContext(ss)
-    sprite(asset.spritesheet,384/2,384/2,384,384)
+    sprite("Project:spritesheet",384/2,384/2,384,384)
     setContext()
     numPics=9    
     pic={}
@@ -24,15 +24,15 @@ function setup()
     pic[7]=ss:copy(192,64,32,32)
     pic[8]=ss:copy(192,96,32,32)
     pic[9]=ss:copy(192,128,32,32)
-    
+
     tabh=0.8*HEIGHT --height of playing area
-    
-    cards={}
+
+cards={}
     ct=1
     local cnt=0
     for i=0,WIDTH-1,WIDTH/row do
         for j=0,tabh-1,tabh/col do
-            table.insert(cards,{x=i+WIDTH/(2*row),y=j+tabh/(2*col),active=1,t=ct})
+        table.insert(cards,{x=i+WIDTH/(2*row),y=j+tabh/(2*col),active=1,t=ct})
             cnt = cnt + 1
             if cnt>=total/numPics then
                 cnt=0
@@ -61,22 +61,22 @@ function draw()
     else
         fill(149, 148, 135, 255)        
     end
-    text(p1.name..": "..p1.score,WIDTH*0.1,HEIGHT*0.9)
+text(p1.name..": "..p1.score,WIDTH*0.1,HEIGHT*0.9)
     if activeplayer==2 then
         fill(255, 243, 0, 255)
     else
         fill(149, 148, 135, 255)        
     end
-    text(p2.name..": "..p2.score,WIDTH*0.9,HEIGHT*0.9)
+text(p2.name..": "..p2.score,WIDTH*0.9,HEIGHT*0.9)
     -- This sets the line thickness
     strokeWidth(5)
-    
-    for i,c in pairs(cards) do
+
+for i,c in pairs(cards) do
         if c.active==1 then
-            sprite("Platformer Art:Block Special Brick",c.x,c.y,0.8*WIDTH/row,0.8*tabh/col)
+        sprite("Platformer Art:Block Special Brick",c.x,c.y,0.8*WIDTH/row,0.8*tabh/col)
         elseif c.active>1 then            
-            sprite(pic[c.t],c.x,c.y,0.8*WIDTH/row,0.8*tabh/col)
-        end
+        sprite(pic[c.t],c.x,c.y,0.8*WIDTH/row,0.8*tabh/col)
+            end
         if c.active>2 then
             c.active = c.active + 1
             if c.active>vistime then
@@ -92,20 +92,20 @@ function draw()
             sound("Game Sounds One:Bell 2")
             if activeplayer==1 then
                 p1.score = p1.score + 1
-            else
+                else
                 p2.score = p2.score + 1
             end
-        else
+            else
             cards[selectIndex1].active=1
             cards[selectIndex2].active=1
             sound("Game Sounds One:Wrong")
             activeplayer = activeplayer + 1
             if activeplayer>2 then activeplayer=1 end
         end
-        selectIndex1=0
-        selectIndex2=0        
+         selectIndex1=0
+         selectIndex2=0        
     end
-    for i,c in pairs(cards) do
+for i,c in pairs(cards) do
         if c.active==0 then
             table.remove(cards,i)
         end
@@ -124,37 +124,37 @@ end
 function touched(t)
     if t.state==ENDED then
         for i,c in pairs(cards) do
-            if c.active==1 and (selectIndex1==0 or selectIndex2==0) and t.x>c.x-WIDTH/(2*row) and t.x<c.x+WIDTH/(2*row) and t.y>c.y-tabh/(2*col) and t.y<c.y+tabh/(2*col) then
-                c.active = c.active + 1
+         if c.active==1 and (selectIndex1==0 or selectIndex2==0) and t.x>c.x-WIDTH/(2*row) and t.x<c.x+WIDTH/(2*row) and t.y>c.y-tabh/(2*col) and t.y<c.y+tabh/(2*col) then
+               c.active = c.active + 1
                 if selectIndex1==0 then
-                    selectIndex1=i
+                selectIndex1=i
                 else
                     selectIndex2=i
                 end
-                if c.active>2 then 
-                    c.active=1 
-                    if selectIndex1==i then
+             if c.active>2 then 
+           c.active=1 
+                if selectIndex1==i then
                         selectIndex1=0
                     end
-                    if selectIndex2==i then
+                if selectIndex2==i then
                         selectIndex2=0
                     end
-                end
+             end
             end
         end
         
         if selectIndex1~=0 and selectIndex2~=0 then
             cards[selectIndex1].active=3
             cards[selectIndex2].active=3
-            
+
         end
         
     end
 end
 
 function exchange()
-    local   c1=math.random(#cards)
-    local  c2=math.random(#cards)
+  local   c1=math.random(#cards)
+   local  c2=math.random(#cards)
     while(c1==c2) do
         c2=math.random(#cards)
     end
