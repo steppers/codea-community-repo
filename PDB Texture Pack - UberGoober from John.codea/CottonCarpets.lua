87a0e319f71c706476877eadfcc7c694f9c1654b
steppers <oliver.steppers@gmail.com>
2021-06-11 23:49:54 +0000

    Delete broken multi-project bundle

--- PDB Texture Pack - UberGoober from John.codea/CottonCarpets.lua
+++
@@ -1,7 +0,0 @@
--- cottons
-function cottonCarpets()  
-    makeFlat("Blue Cotton", packPrefix.."Cotton Blue")
-    makeFlat("Green Cotton", packPrefix.."Cotton Green")
-    makeFlat("Red Cotton", packPrefix.."Cotton Red")
-    makeFlat("Tan Cotton", packPrefix.."Cotton Tan")
-end
