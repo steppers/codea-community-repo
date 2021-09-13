29d06d42d862f558e9c1cd47bf44a87f3a854c70
Oliver Steptoe <oliver.steppers@gmail.com>
2021-09-14 00:38:11 +0100

    Parse more!

--- .github/submit.sh
+++ .github/submit.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-echo "$1" > meta.json
-cat meta.json
-
-sub_name=$(cat meta.json | jq -r '.name')
+sub_name=$(echo "$1" | jq -r '.name')
+sub_desc_short=$(echo "$1" | jq -r '.short_description')
+sub_desc_long=$(echo "$1" | jq -r '.description')
+sub_authors=$(echo "$1" | jq -r '.authors[]')
+sub_version=$(echo "$1" | jq -r '.version')
+sub_update_notes=$(echo "$1" | jq -r '.update_notes')
+sub_zip_url=$(echo "$1" | jq -r '.zip_url')
+sub_library=$(echo "$1" | jq -r '.library')
+sub_hidden=$(echo "$1" | jq -r '.hidden')
 
 echo Processing ${sub_name}...
+
+echo $sub_authors
