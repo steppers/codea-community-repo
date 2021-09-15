4b3002f13e9e8e090af9585bdd9013328c1e9946
Oliver Steptoe <oliver.steppers@gmail.com>
2021-09-15 06:25:26 +0100

    Use files.io rather than Bayfiles

--- .github/submit.sh
+++ .github/submit.sh
@@ -13,13 +13,8 @@ sub_hidden=$(echo "$1" | jq -r '.hidden')
 echo Processing "${sub_name}"...
 echo Using "${sub_zip_url}"
 
-# Get the actual zip url by scraping the bayfiles page
-#sub_zip_url=$(curl -sS ${sub_zip_url} | sed -n 's/.*href="\(https\:\/\/cdn[^"]*\)".*/\1/p')
-sub_zip_url=$(curl -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36" -bjsS ${sub_zip_url})
-echo ${sub_zip_url}
-
 # Download zip file
-curl "${sub_zip_url}" -o submission.zip
+curl -vv "${sub_zip_url}" -o submission.zip
 
 # Extract zip file
 unzip submission.zip -d "${subname}.codea"
