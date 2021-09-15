f9a2da1f841de26d003d10bdf828c8a965de0751
Oliver Steptoe <oliver.steppers@gmail.com>
2021-09-15 20:17:59 +0100

    Commit submitted projects

--- .github/submit.sh
+++ .github/submit.sh
@@ -24,7 +24,7 @@ project_name=$(echo ${sub_name} | tr ' ' '_')
 project_dir="${git_repo}/${project_name}/${project_ver}"
 
 # Delete old project version if it already exists
-[[ -d "${project_dir}" ]] && rm -rf "${project_dir}"
+[[ -d "${project_dir}" ]] && git rm -rf "${project_dir}"
 mkdir -p "${project_dir}"
 
 echo Processing "${sub_name}"...
@@ -42,12 +42,16 @@ cd "${project_dir}"
 mv *.codea "${bundle_name}"
 
 # Generate manifest
-find "${bundle_name}"
+find "${bundle_name}" -type f > manifest.txt
 
 # Sanitize .codea bundle name to match project name + authors
 
 # Adjust project metadata in Info.plist to match provided metadata
 
 # Commit project
+commit_message="Add $sub_name project for submission"
+git add -A
+git commit -m "${commit_message}"
+git push
 
 # Add to submission manifest file
