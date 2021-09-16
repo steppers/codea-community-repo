c7eab1a2a682cd0532339fa1c1565a020f554402
Oliver Steptoe <oliver.steppers@gmail.com>
2021-09-16 19:55:09 +0100

    Test pushover function

--- .github/submit.py
+++ .github/submit.py
@@ -1,3 +1,26 @@
 import os
+import urllib3
 
-print(os.environ.get('GITHUB_WORKSPACE'))
+PUSHOVER_APP_TOKEN = os.environ.get('PUSHOVER_APP_TOKEN')
+PUSHOVER_GROUP_TOKEN = os.environ.get('PUSHOVER_GROUP_TOKEN')
+
+def pushover(title, message):
+    payload = {
+        "token": PUSHOVER_APP_TOKEN,
+        "user": PUSHOVER_GROUP_TOKEN,
+        "title": title,
+        "message": message
+    }
+
+    r = http.request_encode_body(
+        'POST',
+        'https://api.pushover.net/1/messages.json',
+        haders={
+            'Content-Type': 'application/json'
+        },
+        body=json.dumps(payload).encode('utf-8'))
+    return
+
+# print(os.environ.get('GITHUB_WORKSPACE'))
+
+pushover("Title", "Message")
