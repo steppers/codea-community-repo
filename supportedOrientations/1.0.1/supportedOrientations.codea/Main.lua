3333cdabbf4a8ecb7eb78000cbf028234d169fe6
autosub <autosub@webrepo.com>
2021-10-28 00:15:43 +0000

    Approve & add supportedOrientations-1.0.1

---
+++ supportedOrientations/1.0.1/supportedOrientations.codea/Main.lua
@@ -0,0 +1,11 @@
+function setup()
+    supportedOrientations(PORTRAIT_ANY)
+    rectMode(CENTER)
+end
+
+
+function draw()
+    background(183, 224, 179)
+    fill(119, 146, 45)
+    rect(WIDTH/2, HEIGHT/2, WIDTH/3, HEIGHT/1.2)
+end
