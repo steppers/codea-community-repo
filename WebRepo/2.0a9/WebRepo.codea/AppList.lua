d222ed3347ea63566d4a60c51880cfa429987228
autosub <autosub@webrepo.com>
2021-11-02 09:33:09 +0000

    Approve & add WebRepo-2.0a9

---
+++ WebRepo/2.0a9/WebRepo.codea/AppList.lua
@@ -0,0 +1,120 @@
+
+function AppList()
+    local root = Oil.Scroll(0, 0, 1.0, -(50 + layout.safeArea.top))
+    :add_style("bufferBottom", 120)
+    :add_style("padding", 10)
+    :add_pre_updater(function(node)
+        -- Skip unless needed
+        if not node.needs_reposition and node.last_width == WIDTH then
+            return
+        end
+        
+        -- Save current state
+        node.needs_reposition = false
+        node.last_width = WIDTH
+        
+        -- Do the repositioning
+        local padding = node:get_style("padding")
+        if WIDTH < 800 then
+            local cw = WIDTH - (padding * 2)
+            for i,child in ipairs(node.children) do
+                child.w = cw
+                child.x = padding
+                child.y = -(child.h + padding) * i
+            end
+        else
+            local cw = (WIDTH - (padding * 3)) // 2
+            for i,child in ipairs(node.children) do
+                child.w = cw
+                child.x = padding + ((i-1) % 2)*(cw + padding)
+                child.y = -(child.h + padding) * ((i+1) // 2)
+            end
+        end
+    end)
+    
+    function root:add_app(app)
+        local root = Oil.Node(0, 0, 300, 80)
+        root:set_priority(33166368000 - (app.info.timestamp or 1)) -- Sort by timestamp
+        
+        -- App icon (missing by default)
+        local icon = Oil.Icon(10, 10, 60, 60, asset.builtin.Blocks.Missing)
+        :add_style("radius", 16)
+    
+        -- Retrieve the app icon
+        DB.getAppIcon(app, function(img)
+            if not img then return end
+            icon:set_style("tex", img)
+        end)
+        
+        -- Name label
+        local name = Oil.Label(80, -0.001, 1.0, 40, app.info.name, LEFT)
+            :set_style_sheet(UI.THEME.news_internal)
+        
+        -- Desc. label
+        local desc = Oil.Label(80, 0.5, 1.0, 40, app.info.short_description, LEFT)
+            :set_style_sheet(UI.THEME.news_internal_alt)
+            :add_style("fontSize", 16)
+        
+        -- Author label
+        local authors = Oil.Label(80, 0, 1.0, 40, app.authors_str, LEFT)
+            :set_style_sheet(UI.THEME.news_internal_alt)
+            :add_style("fontSize", 16)
+        
+        local function update_button_text(bttn)            
+            if DB.isAppInstalled(app.name, app.version) then
+                bttn:add_style("text", "INSTALLED")
+            elseif not DB.isAppDownloaded(app.name, app.version) then
+                bttn:add_style("text", "GET")
+            else
+                bttn:add_style("text", "INSTALL")
+            end
+        end
+        
+        -- Action button
+        local button = Oil.TextButton(-0.0001, 0.5, 135, 30, "INSTALL", function(bttn)
+            if bttn:get_style("text") == "GET" then
+                bttn:set_style("text", "...")
+                DB.downloadApp(app, function(success, err)
+                    if success then
+                        DB.installApp(app.name, app.version)
+                        update_button_text(bttn)
+                    else
+                        print(err)
+                        bttn:set_style("text", "GET")
+                    end
+                end)
+            elseif bttn:get_style("text") == "INSTALL" then
+                DB.installApp(app.name, app.version)
+                update_button_text(bttn)
+            end
+        end)
+        :set_style_sheet(UI.THEME.button)
+        update_button_text(button)
+        
+        -- 1 pixel line
+        local divider = Oil.Rect(80, 1, -0.001, 1.0001)
+        :set_style_sheet(UI.THEME.divider)
+        
+        root:add_children(
+            icon,
+            name,
+            desc,
+            authors,
+            button,
+            divider
+        )
+        
+        self:add_child(root)
+        
+        
+        root:add_handler(Oil.ButtonHandler(function()
+            -- Open the app window
+            AppWindow(app)
+        end))
+        
+        -- Trigger an update
+        self.needs_reposition = true
+    end
+    
+    return root
+end
---
+++ WebRepo/2.0a9/WebRepo.codea/Main.lua
@@ -0,0 +1,151 @@
+-- WebRepoSub
+
+-- TODO:
+-- Port across old projects
+-- Metadata review
+-- Search tab
+-- Sorted app lists (by name)
+-- Auto-update
+-- Check flow of private keys/passwords
+-- (DONE) iPhone & iPad specific projects
+-- (DONE) Tab animations
+-- (DONE) Handle submissions with dependencies
+-- (DONE) Oil scroll nested clipping
+-- (DONE) Fix buttons on news & app lists
+-- (DONE) Settings screen
+-- (DONE) Approval timestamps (github end)
+-- (DONE) Admin submissions
+-- (DONE) Project sizes
+-- (DONE) Forum-links
+-- (DONE) Remove .version during submission
+-- (DONE) What's New strings
+
+function setup()
+    viewer.mode = FULLSCREEN_NO_BUTTONS
+    viewer.preferredFPS = 60
+    
+    Oil.setup()
+    DB.setup()
+    
+    Oil.root:add_renderer(Oil.RectRenderer)
+        :set_style(UI.THEME.background)
+    
+    -- Top right buttons
+    do
+        local node = Oil.HorizontalStack(-10, -(0.0001 + layout.safeArea.top), 1.0, 50)
+        :set_priority(10)
+        :add_style("align", RIGHT)
+        
+        -- Add review button
+        UI.REVIEW_BUTTON = Oil.TextButton(0, 0.5, 80, 30, "Review", function()
+            ReviewWindow()
+        end)
+        :set_style_sheet(UI.THEME.button)
+        
+        -- Allow it to be disabled
+        UI.REVIEW_BUTTON.enabled = UI.ENABLE_REVIEW
+
+        node:add_child(UI.REVIEW_BUTTON)
+        
+        -- Add other buttons
+        node:add_children(
+            Oil.TextButton(0, 0.5, 80, 30, "Submit", function()
+                SubmitWindow()
+            end)
+            :set_style_sheet(UI.THEME.button),
+        
+            Oil.TextButton(0, 0.5, 80, 30, "Settings", function()
+                SettingsWindow()
+            end)
+            :set_style_sheet(UI.THEME.button)
+        )
+    end
+    
+    -- Top left buttons
+    Oil.HorizontalStack(10, -(0.0001 + layout.safeArea.top), 1.0, 50)
+    :set_priority(10)
+    :add_style("align", LEFT)
+    :add_children(
+        Oil.EmojiButton(0, 0.5, 30, 30, "❌", function()
+            viewer.close()
+        end)
+        :set_style_sheet(UI.THEME.button)
+        :add_style("fontSize", 14)
+        :add_style("textFill", color(255))
+    )
+    
+    -- Bottom bar
+    local news = Oil.Node(0, 0, 1.0, 1.0)
+    local games = AppList()
+    local apps = AppList()
+    local libs = AppList()
+    --local assets = AppList()
+    local search = AppList()
+    CreateTabRow(news, games, apps, libs, --[[assets,]] search)
+    
+    -- Initialise tabs
+    initNewsNode(news)
+    news.hidden = true
+    
+    local onLoadStore = function(store)
+        if store then
+            for name,versions in pairs(store) do
+                -- Only use the latest version
+                local app = store[name][#versions]
+                
+                newsAddItem({
+                    type = (#versions == 1 and "new_project") or "new_update",
+                    app = app
+                })
+                
+                if app.info.category == "App" then
+                    apps:add_app(app)
+                elseif app.info.category == "Game" then
+                    games:add_app(app)
+                elseif app.info.category == "Library" then
+                    libs:add_app(app)
+                elseif app.info.category == "Assets" then
+                    --assets:add_app(app)
+                    libs:add_app(app)
+                end
+            end
+            
+            --apps:sort()
+            --games:sort()
+            --libs:sort()
+        else
+            print("Failed to load store")
+        end
+        news.hidden = false
+    end
+    
+    -- Initialise the backend
+    DB.loadStore(onLoadStore)
+end
+
+function draw()
+    collectgarbage("collect")
+    
+    Oil.beginDraw()
+    Oil.endDraw()
+end
+
+function sizeChanged(w, h)
+    Oil.sizeChanged(w, h)
+end
+
+function hover(g)
+    Oil.hover(g)
+end
+
+function scroll(g)
+    Oil.scroll(g)
+end
+
+function touched(t)
+    Oil.touch(t)
+end
+
+function keyboard(k)
+    Oil.keyboard(k)
+end
